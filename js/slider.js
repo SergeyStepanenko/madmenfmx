dfc0da05b151468761ed10eddaecb8cb1dc03a5d
SergeyStepanenko <groverfmx@gmail.com>
2017-01-10 14:29:56 +0300

    need to undo

--- js/slider.js
+++ js/slider.js
@@ -1,47 +1,63 @@
-; "use strict";
+var swaper = {
 
-var startPoint={};
-var nowPoint;
-var ldelay;
+  // Not sure if keeping element collections like this
+  // together is useful or not.
+  el: {
+    swaper: $("#swaper"),
+    allSlides: $(".slide"),
+    swaperNav: $(".swaper-nav"),
+    allNavButtons: $(".swaper-nav > a")
+  },
 
-document.addEventListener('touchstart', function(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  startPoint.x=event.changedTouches[0].pageX;
-  startPoint.y=event.changedTouches[0].pageY;
-  ldelay = new Date();
-}, false);
+  timing: 800,
+  slideWidth: 300, // could measure this
 
-/*Ловим движение пальцем*/
-document.addEventListener('touchmove', function(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  var otk = {};
-  nowPoint = event.changedTouches[0];
-  otk.x = nowPoint.pageX-startPoint.x;
-  /*Обработайте данные*/
-  /*Для примера*/
-  if(Math.abs(otk.x)>200){
-  if(otk.x<0){/*СВАЙП ВЛЕВО(ПРЕД.СТРАНИЦА)*/}
-  if(otk.x>0){/*СВАЙП ВПРАВО(СЛЕД.СТРАНИЦА)*/}
-  startPoint={x:nowPoint.pageX,y:nowPoint.pageY};
-  }
-}, false);
+  // In this simple example, might just move the
+  // binding here to the init function
+  init: function() {
+    this.bindUIEvents();
+  },
 
-/*Ловим отпускание пальца*/
-document.addEventListener('touchend', function(event) {
-  var pdelay=new Date();
-  nowPoint=event.changedTouches[0];
-  var xAbs = Math.abs(startPoint.x - nowPoint.pageX);
-  var yAbs = Math.abs(startPoint.y - nowPoint.pageY);
-  if ((xAbs > 20 || yAbs > 20) && (pdelay.getTime()-ldelay.getTime())<200) {
-  if (xAbs > yAbs) {
-  if (nowPoint.pageX < startPoint.x){/*СВАЙП ВЛЕВО*/}
-  else{/*СВАЙП ВПРАВО*/}
+  bindUIEvents: function() {
+    // You can either manually scroll...
+    this.el.swaper.on("scroll", function(event) {
+      swaper.moveSlidePosition(event);
+    });
+    // ... or click a thing
+    this.el.swaperNav.on("click", "a", function(event) {
+      swaper.handleNavClick(event, this);
+    });
+    // What would be cool is if it had touch
+    // events where you could swipe but it
+    // also kinda snapped into place.
+  },
+
+  moveSlidePosition: function(event) {
+    // Magic Numbers =(
+    this.el.allSlides.css({
+      "background-position": $(event.target).scrollLeft()/6-100+ "px 0"
+    });
+  },
+
+  handleNavClick: function(event, el) {
+    event.preventDefault();
+    var position = $(el).attr("href").split("-").pop();
+
+    this.el.swaper.animate({
+      scrollLeft: position * this.slideWidth
+    }, this.timing);
+
+    this.changeActiveNav(el);
+  },
+
+  changeActiveNav: function(el) {
+    this.el.allNavButtons.removeClass("active");
+    $(el).addClass("active");
   }
-  else {
-  if (nowPoint.pageY < startPoint.y){/*СВАЙП ВВЕРХ*/}
-  else{/*СВАЙП ВНИЗ*/}
-  }
-  }
-}, false);
+
+};
+
+swaper.init();
+
+// http://codepen.io/BaylorRae/pen/ImGBC
+// Originally added click links, so I ported over and re-wrote
