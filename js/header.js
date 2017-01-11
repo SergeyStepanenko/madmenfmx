47d8bb7803b8f2278daf79dafd50a118da1b1c0a
SergeyStepanenko <groverfmx@gmail.com>
2017-01-11 15:17:24 +0300

    updated header using transform

--- js/header.js
+++ js/header.js
@@ -1,40 +1,64 @@
 ; "use strict";
 
-var menuStatus = 0;
-var header = document.getElementById('headerForSmallDevices');
-var i = 0;
-var delay = 1;
-var speedOfAnimation = 10;
+var headerStatus = 0;
 
 document.querySelectorAll(".headerButton")[0].onclick = function () {
-  if (menuStatus == 0) {
-    header.style.display = 'flex';
-    (function () {
-      function startTimer () {
-        if (i < 301) {
-          header.style.height = i + 'px';
-          setTimeout(startTimer, delay);
-        };
-      i += speedOfAnimation;
-      };
-    startTimer ();
-  }());
-
-    menuStatus = 1;
-
+  if (headerStatus == 0) {
+    document.querySelectorAll(".headerForSmallDevices")[0].style.WebkitTransform = "translate(0, 300px)";
+    document.querySelectorAll(".headerForSmallDevices")[0].style.msTransform = "translate(0, 300px)";
+    document.querySelectorAll(".headerForSmallDevices")[0].style.Transform = "translate(0, 300px)";
+    headerStatus = 1;
   } else {
-    (function () {
-      function startTimer () {
-        if (i > -1) {
-          header.style.height = i + 'px';
-          setTimeout(startTimer, delay);
-        };
-      i -= speedOfAnimation;
-      };
-    startTimer ();
-  }());
+    document.querySelectorAll(".headerForSmallDevices")[0].style.WebkitTransform = "translate(0, 0)";
+    document.querySelectorAll(".headerForSmallDevices")[0].style.msTransform = "translate(0, 0)";
+    document.querySelectorAll(".headerForSmallDevices")[0].style.Transform = "translate(0, 0)";
+    headerStatus = 0;
+  }
+}
 
-    menuStatus = 0;
 
-  };
-};
+
+
+
+
+
+// HEADER OLD VER
+
+// var menuStatus = 0;
+// var header = document.getElementById('headerForSmallDevices');
+// var i = 0;
+// var delay = 1;
+// var speedOfAnimation = 10;
+//
+// document.querySelectorAll(".headerButton")[0].onclick = function () {
+//   if (menuStatus == 0) {
+//     header.style.display = 'flex';
+//     (function () {
+//       function startTimer () {
+//         if (i < 301) {
+//           header.style.height = i + 'px';
+//           setTimeout(startTimer, delay);
+//         };
+//       i += speedOfAnimation;
+//       };
+//     startTimer ();
+//   }());
+//
+//     menuStatus = 1;
+//
+//   } else {
+//     (function () {
+//       function startTimer () {
+//         if (i > -1) {
+//           header.style.height = i + 'px';
+//           setTimeout(startTimer, delay);
+//         };
+//       i -= speedOfAnimation;
+//       };
+//     startTimer ();
+//   }());
+//
+//     menuStatus = 0;
+//
+//   };
+// };
