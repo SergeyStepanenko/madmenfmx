15f6efd03f5923b39cfc9137bf111952f07e3a48
SergeyStepanenko <groverfmx@gmail.com>
2017-01-30 22:03:49 +0300

    123

--- js/header.js
+++ js/header.js
@@ -1,1 +1,43 @@
 ; "use strict";
+
+var headerStatus = 0;
+var mobileMenuButton = document.querySelectorAll(".mobileMenuButton");
+
+
+document.querySelectorAll(".headerButton")[0].onclick = function () {
+  if (headerStatus == 0) {
+    document.querySelector(".headerForSmallDevices").style.WebkitTransform = "translate3d(0, 325px, 0)";
+    document.querySelector(".headerForSmallDevices").style.msTransform = "translate3d(0, 325px, 0)";
+    document.querySelector(".headerForSmallDevices").style.Transform = "translate3d(0, 325px, 0)";
+    document.querySelector(".contentWrapper").style.WebkitTransform = "translate3d(0, 325px, 0)";
+    document.querySelector(".contentWrapper").style.msTransform = "translate3d(0, 325px, 0)";
+    document.querySelector(".contentWrapper").style.Transform = "translate3d(0, 325px, 0)";
+    headerStatus = 1;
+  } else {
+    document.querySelector(".headerForSmallDevices").style.WebkitTransform = "translate3d(0, 0, 0)";
+    document.querySelector(".headerForSmallDevices").style.msTransform = "translate3d(0, 0, 0)";
+    document.querySelector(".headerForSmallDevices").style.Transform = "translate3d(0, 0, 0)";
+    document.querySelector(".contentWrapper").style.WebkitTransform = "translate3d(0, 0, 0)";
+    document.querySelector(".contentWrapper").style.msTransform = "translate3d(0, 0, 0)";
+    document.querySelector(".contentWrapper").style.Transform = "translate3d(0, 0, 0)";
+    headerStatus = 0;
+  }
+};
+
+for (var i = 0; i < mobileMenuButton.length; i++) {
+  document.querySelectorAll(".mobileMenuButton")[i].onclick = function () {
+    headerStatus = 0;
+  }
+};
+
+// for (var i = 0; i < mobileMenuButton.length; i++) {
+//   document.querySelectorAll(".mobileMenuButton")[i].onclick = function () {
+//     document.querySelector(".headerForSmallDevices").style.WebkitTransform = "translate3d(0, 0, 0)";
+//     document.querySelector(".headerForSmallDevices").style.msTransform = "translate3d(0, 0, 0)";
+//     document.querySelector(".headerForSmallDevices").style.Transform = "translate3d(0, 0, 0)";
+//     document.querySelector(".contentWrapper").style.WebkitTransform = "translate3d(0, 0, 0)";
+//     document.querySelector(".contentWrapper").style.msTransform = "translate3d(0, 0, 0)";
+//     document.querySelector(".contentWrapper").style.Transform = "translate3d(0, 0, 0)";
+//     headerStatus = 0;
+//   }
+// };
