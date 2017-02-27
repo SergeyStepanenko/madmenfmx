d89bd5f677fe8408823c83cb444f7d8fb40a9dfb
SergeyStepanenko <groverfmx@gmail.com>
2017-02-27 14:24:33 +0300

    123

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,7 @@
 ; "use strict";
 
 var allImgs = document.querySelectorAll(".smallImage");
+var gall = document.querySelector(".gallery");
 var maxImgNumber = allImgs.length;
 var sliderImgNum;
 var width = document.body.clientWidth;
@@ -36,6 +37,73 @@ document.querySelectorAll(".closePopup")[0].onclick = function() {
 
   leftArrow.addEventListener("click", moveBackward);
   rightArrow.addEventListener("click", moveForward);
+
+  gall.addEventListener("wheel", function onWheel(e) {
+    e = e || window.event;
+    var delta = e.deltaY || e.detail || e.wheelDelta;
+    delta == 100 ? moveForward() : moveBackward();
+    e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+  });
+
+
+
+  gall.onkeydown = gall.onkeyup = gall.onkeypress = handle;
+
+  var lastTime = Date.now();
+
+  function handle(e) {
+    if (form.elements[e.type + 'Ignore'].checked) return;
+
+    var text = e.type +
+      ' keyCode=' + e.keyCode +
+      ' which=' + e.which +
+      ' charCode=' + e.charCode +
+      ' char=' + String.fromCharCode(e.keyCode || e.charCode) +
+      (e.shiftKey ? ' +shift' : '') +
+      (e.ctrlKey ? ' +ctrl' : '') +
+      (e.altKey ? ' +alt' : '') +
+      (e.metaKey ? ' +meta' : '') + "\n";
+
+    if (area.value && Date.now() - lastTime > 250) {
+      console.log(area.value += new Array(81).join('-') + '\n');;
+    }
+    lastTime = Date.now();
+
+    console.log(area.value += text);;
+
+    if (form.elements[e.type + 'Stop'].checked) {
+      e.preventDefault();
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  gall.addEventListener("keydown", function(event) {
+    console.log('hi');
+        }, false);
+
+
+
+
+
+  function onWheel(e) {
+    e = e || window.event;
+    var delta = e.deltaY || e.detail || e.wheelDelta;
+    delta == 100 ? moveForward() : moveBackward();
+    e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+  };
+
+
 
   function moveBackward (){
     var img = document.getElementById("img");
